5651580ad929cd1ef2b8e7cd725dd5b39da1d056
ZuiYee <39691460+ZuiYee@users.noreply.github.com>
2018-12-31 14:50:25 +0800

    12.31.1

--- web/urls.py
+++ web/urls.py
@@ -12,7 +12,7 @@ app_name = 'web'
 urlpatterns = [
     path('admin/', admin.site.urls),
     url(r'^profile/', views.profile, name='profile'),
-    url(r'^blogHome/', views.blogHome, name='blogHome'),
+    url(r'^blogHome/$', views.blogHome, name='blogHome'),
     url(r'^detail/(?P<key>.*?)$', views.detail, name='detail'),
 
 ]
--- web/views.py
+++ web/views.py
@@ -1,21 +1,40 @@
 from django.shortcuts import render
 from .models import Article
 from django.utils.safestring import mark_safe
-allType = []
+
 Month = {'01': "Jan", '02': "Feb", '03': "Mar", '04': "Apr", '05': "May", '06': "Jun", '07': "Jul",
-                '08': "Aug", '09': "Sep", '10': "Oct", '11': "Nov", '12': "Dec"}
-find = None
+         '08': "Aug", '09': "Sep", '10': "Oct", '11': "Nov", '12': "Dec"}
+
+
+def recent():
+    find = Article.objects.all()
+    recentFind = []
+    recentFind.append(find.last())
+    x = find.filter(pk=find.last().pk - 1)
+    if x:
+        recentFind.append(find.filter(pk=find.last().pk - 1)[0])
+    y = find.filter(pk=find.last().pk - 2)
+    if y:
+        recentFind.append(find.filter(pk=find.last().pk - 2)[0])
+    for item in recentFind:
+        date = item.time.strftime("%Y-%m-%d").split('-')
+        if date:
+            item.year = date[0]
+            item.mon = Month[date[1]]
+            item.day = date[2]
+    return recentFind
+
 
 def summary():
-    global allType
+    allType = []
     allData = Article.objects.all()
     for item in allData:
         allType.append(item.type)
     allType = sorted(set(allType), key=allType.index)
+    return allType
 
 
-def Paging(page=1, find=None):
-    global allType
+def Paging(page=1, find=None, articleType=None):
     pageNum = 3
     context = {}
     start = (page - 1) * pageNum
@@ -47,55 +66,85 @@ def Paging(page=1, find=None):
             if (page + (pageNum - 1) / 2) > pageCount:
                 endIndex = pageCount + 1
                 startIndex = pageCount - pageNum + 1
-    if page == 1:
-        prev = '<li><a href="javascript:void(0);">&laquo;</a></li>'
-    else:
-        prev = '<li><a href="/web/blogHome/?p=%s">&laquo;</a></li>' % (page - 1)
-    pageList.append(prev)
-    for i in range(int(startIndex), int(endIndex)):
-        if i == page:
-            temp = '<li><a class="active" href="/web/blogHome/?p=%s">%s</a></li>' % (i, i)
+    if articleType:
+        if page == 1:
+            prev = '<li><a href="javascript:void(0);">&laquo;</a></li>'
         else:
-            temp = '<li><a href="/web/blogHome/?p=%s">%s</a></li>' % (i, i)
-        pageList.append(temp)
-    if page == pageCount:
-        nex = '<li><a href="javascript:void(0);">&raquo;</a></li>'
+            prev = '<li><a href="/web/blogHome/?type=%s&p=%s">&laquo;</a></li>' % (articleType, page - 1)
+        pageList.append(prev)
+        for i in range(int(startIndex), int(endIndex)):
+            if i == page:
+                temp = '<li><a class="active" href="/web/blogHome/?type=%s&p=%s">%s</a></li>' % (articleType, i, i)
+            else:
+                temp = '<li><a href="/web/blogHome/?type=%s&p=%s">%s</a></li>' % (articleType, i, i)
+            pageList.append(temp)
+        if page == pageCount:
+            nex = '<li><a href="javascript:void(0);">&raquo;</a></li>'
+        else:
+            nex = '<li><a href="/web/blogHome/?type=%s&p=%s">&raquo;</a></li>' % (articleType, page + 1)
+        pageList.append(nex)
+
+        jump = """
+            <input type="text"  class="jump" /><button class="btn btn-default" onclick='jumpTo(this, "/web/blogHome/?type=%s&p=");' id="jumpPageNum">Go</button>
+            <script>
+                function jumpTo(ths, base){
+                    var val = ths.previousSibling.value;
+                    location.href = base + val;
+                }
+            </script>
+        """%(articleType)
+        pageList.append(jump)
+
+        pageStr = "".join(pageList)
+        pageStr = mark_safe(pageStr)
+        allType = summary()
+        context['find'] = data
+        context['pageStr'] = pageStr
+        context['allType'] = allType
+        return context
     else:
-        nex = '<li><a href="/web/blogHome/?p=%s">&raquo;</a></li>' % (page + 1)
-    pageList.append(nex)
+        if page == 1:
+            prev = '<li><a href="javascript:void(0);">&laquo;</a></li>'
+        else:
+            prev = '<li><a href="/web/blogHome/?p=%s">&laquo;</a></li>' % (page - 1)
+        pageList.append(prev)
+        for i in range(int(startIndex), int(endIndex)):
+            if i == page:
+                temp = '<li><a class="active" href="/web/blogHome/?p=%s">%s</a></li>' % (i, i)
+            else:
+                temp = '<li><a href="/web/blogHome/?p=%s">%s</a></li>' % (i, i)
+            pageList.append(temp)
+        if page == pageCount:
+            nex = '<li><a href="javascript:void(0);">&raquo;</a></li>'
+        else:
+            nex = '<li><a href="/web/blogHome/?p=%s">&raquo;</a></li>' % (page + 1)
+        pageList.append(nex)
 
-    jump = """
-        <input type="text"  class="jump" /><button class="btn btn-default" onclick='jumpTo(this, "/web/blogHome/?p=");' id="jumpPageNum">Go</button>
-        <script>
-            function jumpTo(ths, base){
-                var val = ths.previousSibling.value;
-                location.href = base + val;
-            }
-        </script>
-    """
-    pageList.append(jump)
+        jump = """
+            <input type="text"  class="jump" /><button class="btn btn-default" onclick='jumpTo(this, "/web/blogHome/?p=");' id="jumpPageNum">Go</button>
+            <script>
+                function jumpTo(ths, base){
+                    var val = ths.previousSibling.value;
+                    location.href = base + val;
+                }
+            </script>
+        """
+        pageList.append(jump)
 
-    pageStr = "".join(pageList)
-    pageStr = mark_safe(pageStr)
-    context['find'] = data
-    context['pageStr'] = pageStr
-    context['allType'] = allType
-    return context
+        pageStr = "".join(pageList)
+        pageStr = mark_safe(pageStr)
+        allType = summary()
+        context['find'] = data
+        context['pageStr'] = pageStr
+        context['allType'] = allType
+        return context
 
 
-def Type(request, type):
-    find = Article.objects.filter(type=type)
+def Type(request, page=1, articleType=None):
+    find = Article.objects.filter(type=articleType)
     if find:
-        context = Paging(1, find)
-        allArticle = Article.objects.all()
-        recentFind = []
-        recentFind.append(allArticle.last())
-        x = allArticle.filter(pk=allArticle.last().pk - 1)
-        if x:
-            recentFind.append(allArticle.filter(pk=allArticle.last().pk - 1)[0])
-        y = allArticle.filter(pk=allArticle.last().pk - 2)
-        if y:
-            recentFind.append(allArticle.filter(pk=allArticle.last().pk - 2)[0])
+        context = Paging(page, find, articleType)
+        recentFind = recent()
         context['recentFind'] = recentFind
         return render(request, 'web/blogHome.html', context)
     else:
@@ -103,49 +152,20 @@ def Type(request, type):
 
 
 def profile(request):
-    global allType, find
     context = {}
-    if request.method == 'POST':
-        if request.POST.get("type"):
-            return Type(request, request.POST.get("type"))
-    if request.method == 'GET':
-        if request.GET.get("s"):
-            find = Article.objects.filter(title__contains=request.GET.get("s"))
-            if find:
-                context = Paging(1, find)
-                return render(request, 'web/profile.html', context)
-            else:
-                return render(request, 'web/error.html')
-        if request.GET.get("p"):
-            page = int(request.GET.get("p", 1))
-            context = Paging(page, find)
-
-            return render(request, 'web/profile.html', context)
-
-    summary()
     find = Article.objects.all()
-    newfind = []
-    newfind.append(find.last())
-    x = find.filter(pk=find.last().pk-1)
-    if x:
-        newfind.append(find.filter(pk=find.last().pk-1)[0])
-    y = find.filter(pk=find.last().pk-2)
-    if y:
-        newfind.append(find.filter(pk=find.last().pk-2)[0])
-
+    newfind = recent()
     if find:
         context['find'] = newfind
-        # context = Paging(1, find)
         return render(request, 'web/profile.html', context)
     else:
         return render(request, 'web/error.html')
 
 
-
 def detail(request, key):
     if request.method == 'POST':
         if request.POST.get("type"):
-            return Type(request, request.POST.get("type"))
+            return Type(request, 1, request.POST.get("type"))
     context = {}
     find = Article.objects.filter(id=key)[0]
     if find:
@@ -154,17 +174,11 @@ def detail(request, key):
             find.year = date[0]
             find.mon = Month[date[1]]
             find.day = date[2]
+        allType = summary()
         context['allType'] = allType
         context['find'] = find
         allArticle = Article.objects.all()
-        recentFind = []
-        recentFind.append(allArticle.last())
-        x = allArticle.filter(pk=allArticle.last().pk - 1)
-        if x:
-            recentFind.append(allArticle.filter(pk=allArticle.last().pk - 1)[0])
-        y = allArticle.filter(pk=allArticle.last().pk - 2)
-        if y:
-            recentFind.append(allArticle.filter(pk=allArticle.last().pk - 2)[0])
+        recentFind = recent()
         context['recentFind'] = recentFind
         return render(request, 'web/detail.html', context)
     else:
@@ -172,11 +186,10 @@ def detail(request, key):
 
 
 def blogHome(request):
-    global allType, find
     context = {}
     if request.method == 'POST':
         if request.POST.get("type"):
-            return Type(request, request.POST.get("type"))
+            return Type(request, 1, request.POST.get("type"))
     if request.method == 'GET':
         if request.GET.get("s"):
             find = Article.objects.filter(title__contains=request.GET.get("s"))
@@ -186,30 +199,26 @@ def blogHome(request):
             else:
                 return render(request, 'web/error.html')
         if request.GET.get("p"):
-            page = int(request.GET.get("p", 1))
-            context = Paging(page, find)
-            allArticle = Article.objects.all()
-            recentFind = []
-            recentFind.append(allArticle.last())
-            x = allArticle.filter(pk=allArticle.last().pk - 1)
-            if x:
-                recentFind.append(allArticle.filter(pk=allArticle.last().pk - 1)[0])
-            y = allArticle.filter(pk=allArticle.last().pk - 2)
-            if y:
-                recentFind.append(allArticle.filter(pk=allArticle.last().pk - 2)[0])
-            context['recentFind'] = recentFind
-            return render(request, 'web/blogHome.html', context)
+            if request.GET["type"]:
+                page = int(request.GET.get("p", 1))
+                print(page, request.GET["type"])
+                Type(request, page, request.GET["type"])
+                # context = Paging(page, find)
+                # allArticle = Article.objects.all()
+                # recentFind = recent()
+                # context['recentFind'] = recentFind
+                # return render(request, 'web/blogHome.html', context)
+            else:
+                page = int(request.GET.get("p", 1))
+                find = Article.objects.all()
+                context = Paging(page, find)
+                allArticle = Article.objects.all()
+                recentFind = recent()
+                context['recentFind'] = recentFind
+                return render(request, 'web/blogHome.html', context)
     summary()
-
     find = Article.objects.all()
-    recentFind = []
-    recentFind.append(find.last())
-    x = find.filter(pk=find.last().pk - 1)
-    if x:
-        recentFind.append(find.filter(pk=find.last().pk - 1)[0])
-    y = find.filter(pk=find.last().pk - 2)
-    if y:
-        recentFind.append(find.filter(pk=find.last().pk - 2)[0])
+    recentFind = recent()
     if find:
         context = Paging(1, find)
         context['recentFind'] = recentFind
